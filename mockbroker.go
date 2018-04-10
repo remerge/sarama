35bdb5ef0ed083eaef9d500f4a95303f0c2662ac
Georgy Buranov <gburanov@gmail.com>
2018-04-10 20:59:23 +0200

    fix

--- mockbroker.go
+++ mockbroker.go
@@ -343,7 +343,7 @@ func (b *MockBroker) defaultRequestHandler(req *request) (res encoder) {
 		b.matchRequestAndResponse(req, res)
 		return res
 	case <-time.After(b.expectationTimeout):
-		b.t.Errorf("Timeout when requesting %v", reflect.TypeOf(req.body).Elem().Name())
+		fmt.Printf("Timeout when requesting %v for %v", reflect.TypeOf(req.body).Elem().Name(), b.BrokerID())
 		return nil
 	}
 }
