a37ebcec6c5a61cceb2b2e7658cbbd271c7b5e0a
Georgy Buranov <gburanov@gmail.com>
2018-04-12 10:59:42 +0200

    use require instead of assert

--- topic_and_broker_test.go
+++ topic_and_broker_test.go
@@ -3,18 +3,18 @@ package sarama
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSimpleTopicAndBrokerUsage(t *testing.T) {
 	subject := TopicAndBroker{}
 	err := subject.Set("test:localhost:9092")
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	err = subject.Set("test2:localhost:9092")
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
-	assert.True(t, subject.Available())
-	assert.False(t, subject.IsEmpty())
-	assert.Equal(t, subject.FirstBrokers(), "localhost:9092")
-	assert.Equal(t, subject.FirstTopic(), "test")
+	require.True(t, subject.Available())
+	require.False(t, subject.IsEmpty())
+	require.Equal(t, subject.FirstBrokers(), "localhost:9092")
+	require.Equal(t, subject.FirstTopic(), "test")
 }
