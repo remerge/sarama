23698799bd38a60a6cf29b09820cdace004b997e
Georgy Buranov <georgy@remerge.io>
2018-04-12 11:07:03 +0200

    Add TopicAndBroker from dm kafka (#11)

---
+++ topic_and_broker_test.go
@@ -0,0 +1,20 @@
+package sarama
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSimpleTopicAndBrokerUsage(t *testing.T) {
+	subject := TopicAndBroker{}
+	err := subject.Set("test:localhost:9092")
+	require.Nil(t, err)
+	err = subject.Set("test2:localhost:9092")
+	require.Nil(t, err)
+
+	require.True(t, subject.Available())
+	require.False(t, subject.IsEmpty())
+	require.Equal(t, subject.FirstBrokers(), "localhost:9092")
+	require.Equal(t, subject.FirstTopic(), "test")
+}
